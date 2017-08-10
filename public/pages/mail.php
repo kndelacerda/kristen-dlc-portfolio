64f9add866f17ed78786fa0f12ef655bc1830540
Kristen De La Cerda <kndelacerda@gmail.com>
2017-08-10 12:54:32 -0500

    Added php

---
+++ public/pages/mail.php
@@ -0,0 +1,12 @@
+
+<?php
+$name = $_POST['name'];
+$email = $_POST['email'];
+$message = $_POST['message'];
+$formcontent=" From: $name \n Email: $email \n Message: $message";
+$recipient = "kndelacerda@gmail.com";
+$subject = "Hello Kristen";
+$mailheader = "From: $email \r\n";
+mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
+echo "Thank You!";
+?>
